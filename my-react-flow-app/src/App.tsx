a3debeae6447f2c0c1db293b68de7719b078298b
deepanshu-QM <cdeepanshu231@gmail.com>
2026-03-23 00:18:59 +0530

    Setting -up Components

---
+++ my-react-flow-app/src/App.tsx
@@ -0,0 +1,14 @@
+import '@xyflow/react/dist/style.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import {CreateWorkFlow} from './components/CreateWorkFlow';
+
+export default App(){
+  return 
+  <div>
+      <BrowserRouter>
+         <Routes>
+           <Route path="./dashboard" element={<CreateWorkFlow/>}/>
+         </Routes>
+      </BrowserRouter>
+  </div>
+}
